29d240204fcc43aafc4e838e191a5e0b9c66c61c
Omar E. Negm <oenegm@gmail.com>
2021-09-19 14:50:14 +0200

    improved dashboard logic, added users brief

--- WalmartInstagram/Controllers/HomeController.cs
+++ WalmartInstagram/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ namespace WalmartInstagram.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult Index()
+        {
+            return RedirectToAction("allposts", "post");
+        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Contact Us.";
--- WalmartInstagram/Controllers/adminController.cs
+++ WalmartInstagram/Controllers/adminController.cs
@@ -17,14 +17,12 @@ namespace WalmartInstagram.Controllers
             ViewBag.userscount = db.users.Count() - 1;
             ViewBag.postscount = db.posts.Count();
 
-            ViewBag.animalscount = db.posts.Where(n => n.category.categoryID == 0).Count();
-            ViewBag.artcount = db.posts.Where(n => n.category.categoryID == 1).Count();
-            ViewBag.buildingsscount = db.posts.Where(n => n.category.categoryID == 2).Count();
-            ViewBag.foodcount = db.posts.Where(n => n.category.categoryID == 4).Count();
-            ViewBag.vehiclescount = db.posts.Where(n => n.category.categoryID == 5).Count();
-            ViewBag.sportsscount = db.posts.Where(n => n.category.categoryID == 6).Count();
-            ViewBag.landscount = db.posts.Where(n => n.category.categoryID == 7).Count();
-
+            List<category> cs = db.categories.ToList();
+            return View(cs);
+        }
+        // do this
+        public ActionResult usersBrief()
+        {
             return View();
         }
     }
